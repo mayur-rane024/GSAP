c00d4038cda116788a96d2e2dad4c2466ff950ed
Mayuresh Rane <ranemayuresh075@gmail.com>
2025-02-26 10:28:49 +0530

    Text animations

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-gsap.to("#box1",{
-    x: 1200,
-    // y: 200,
-    duration: 1.5,
-    delay : 0.6,
-    rotate : 3500,
-    backgroundColor : "teal",         // CAMEL CASE USE KARO SINCE JS
-    borderRadius : "50%",
-    scale : 2.5
-})
-gsap.from("#box2",{
-    x: 1200,
-    // y: -200,
-    duration: 2,
-    delay : 0.6,
-    rotate : 3500,
-    backgroundColor : "teal",
-    borderRadius : "0%",
-    scale : 0.5
+gsap.from("h1" , {
+    opacity : 0 ,
+    x : 200,
+    color: "red" , 
+    duration : 1.5 ,
+    delay : 2
 })
